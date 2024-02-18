1d1de15f1d8a8fefcb23b588d1aeadc295a62b50
Administrator <batinsavran00@gmail.com>
2024-02-18 23:37:37 +0300

    first commit

---
+++ app/build/generated/source/buildConfig/debug/com/google/ai/sample/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.ai.sample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.ai.sample";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String apiKey = "AIzaSyDsvevumaw2vuS_WMGdAf73hcJk83NElqQ";
+}
